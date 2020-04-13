0f6b237b0c0a6bd74fdfb7ccc0a442ff6b2f9f87
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-04-13 20:18:19 +0100

    Remove all the example app source now it has been moved to refactoring-to-kotlin-code

--- code/src/main/java/chapter1/kotlin15/Sessions.15.kt
+++
@@ -1,8 +0,0 @@
-package chapter1.kotlin15
-
-import chapter1.kotlin8.Session
-
-object Sessions {
-    fun List<Session>.firstWithTitle(title: String) =
-        firstOrNull { it.title == title }
-}
--- code/src/main/java/chapter1/kotlin2/Presenter.2.kt
+++
@@ -1,6 +0,0 @@
-package chapter1.kotlin2
-
-
-data class Presenter(
-    val id: String,
-    val name: String)
