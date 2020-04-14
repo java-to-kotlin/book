42bdbd835f612102e17b39e3a8508ee6875b3b7e
Nat Pryce <sw@natpryce.com>
2020-04-14 22:07:02 +0100

    Build script runs tests and snippetty stuff before processing asciidoc, unless the --fast argument is given.

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 set -e
-image=asciidoctor/docker-asciidoctor
-docker run --rm --tty -v "$PWD"/:/documents/ $image make "$@"
+
+if [ "$1" = "--fast" ]
+then
+  shift
+else
+  # test the code
+  ./gradlew code:test
+  (cd ../refactoring-to-kotlin-code && ./gradlew test)
+
+  # include the code in the Asciidoc
+  ./gradlew build
+fi
+
+# build the book
+docker run --rm --tty -v "$PWD"/:/documents/ asciidoctor/docker-asciidoctor make "$@"
