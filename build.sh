6d688a1354781bf5206417106ed1fa02f42e56fb
Nat Pryce <sw@natpryce.com>
2020-04-21 23:13:15 +0100

    Do snippeting in build.sh

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ else
   (cd ../refactoring-to-kotlin-code && ./gradlew --warning-mode=all $clean test)
 
   # include the code in the Asciidoc
+  ./retag-worked-example --for-real
   ./gradlew  --warning-mode=all build
 fi
 
