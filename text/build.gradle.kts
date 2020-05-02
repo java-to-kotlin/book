b14bdb3b69495c7a253443e404263a48c8fc8871
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-05-02 16:47:29 +0100

    Build tidy

--- text/build.gradle.kts
+++ text/build.gradle.kts
@@ -1,17 +1,24 @@
 import book.processFiles
+import book.runCommand
 
 tasks {
     create("build") {
         doFirst {
             processFiles(
-                textRoot =
-                    project.projectDir,
-                workedExampleSrcRoot =
-                    project.rootProject.projectDir.resolve("../refactoring-to-kotlin-code").canonicalFile,
-                digressionSrcRoot =
-                    project.rootProject.projectDir.resolve("code").canonicalFile,
+                textRoot = project.projectDir,
+                workedExampleSrcRoot = project.rootProject.projectDir.resolve("../refactoring-to-kotlin-code"),
+                digressionSrcRoot = project.rootProject.projectDir.resolve("code"),
                 abortOnFailure = true
             )
         }
     }
+
+// This causes us not to find the tags when "build" is run, even though it has completed
+//    create("clean") {
+//        doFirst {
+//            "./retag-worked-example".runCommand(workingDir = project.rootProject.projectDir)
+//            Thread.sleep(2000)
+//        }
+//    }
 }
+
