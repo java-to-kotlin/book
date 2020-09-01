7fd0260ad5bcb322e2691936fe2ca53aa1b935a2
Nat Pryce <sw@natpryce.com>
2020-09-01 16:42:36 +0100

    Delete old build stuff

--- build.sh
+++ build.sh
@@ -1,26 +1,4 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "--fast" ]
-then
-  shift
-else
-  if [ "$1" = "clean" ]
-  then
-    clean=clean
-  else
-    clean=
-  fi
-
-  # test the code
-  ./gradlew --warning-mode=all $clean code:test
-
-  # include the code in the Asciidoc
-  ./gradlew --warning-mode=all build
-
-  # build the book-build book build docker container
-  ./docker/book-build/build.sh
-fi
-
-# build the book
-docker run --rm --tty -v "$PWD"/:/documents/ book-build make $clean "$@"
+./gradlew --warning-mode=all clean code:test build
--- docker/book-build/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e
-docker build --tag="$(basename "$(dirname $0)")" "$(dirname $0)"
