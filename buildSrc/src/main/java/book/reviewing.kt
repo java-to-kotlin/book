7d52e8c88f089e73903bfa4d17eb2a3dcb10ce50
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-10-17 21:21:46 +0100

    Add code to convert Atlas rendered html a format that can be pasted in GDocs from Chrome

--- buildSrc/src/main/java/book/reviewing.kt
+++ buildSrc/src/main/java/book/reviewing.kt
@@ -1,66 +1,99 @@
 package book
 
 import org.w3c.dom.Document
-import org.w3c.dom.NamedNodeMap
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import java.io.File
+import java.io.StringWriter
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
 
 fun main() {
-    val f = File("/Users/duncan/Downloads/3c7d4023e8a6f5c00efc53a6/content.html")
-//    val t = Tidy().apply {
-//        xhtml = true
-//    }
-//    val doc = f.inputStream().use {
-//        t.parseDOM(it, null)
-//    }
+    val inFile = File("/Users/duncan/Downloads/a337be3acd1c47b0e117f650/book.html")
+    val cssFile = File("buildSrc/src/main/java/book/html.css")
+    val outDir = File("./build/gdocs-import")
 
-
-    val dbFactory = DocumentBuilderFactory.newInstance()
-    val dBuilder =  dbFactory.newDocumentBuilder()
-    val doc: Document = dBuilder.parse(f)
-    doc.documentElement.normalize()
-    doc.renameAllElements("h6", "h7")
-    doc.renameAllElements("h5", "h6")
-    doc.renameAllElements("h4", "h5")
-    doc.renameAllElements("h3", "h4")
-    doc.renameAllElements("h2", "h3")
-
-    TransformerFactory.newInstance().newTransformer().transform(
-        DOMSource(doc), StreamResult(File("delme.html"))
-    )
-
-
-//
-
-
-    doc.getElementsByTagName("h1").toList().forEach() {
-
-        println("${it.nodeName} ${it.childNodes.item(0).nodeValue}")
-    }
-//    chapterSections.forEach { node ->
-//
-//        node.replaceChild()
-//        println("${node.nodeName} ${node.attributes.toMap()}")
-//    }
+    render(inFile, cssFile, outDir)
 }
+
+fun render(inFile: File, cssFile: File, outDir: File) {
+    val xml = inFile.readLines(Charsets.UTF_8).fixBrokenTags()
+    val doc = xml.joinToString("\n").toDocument().fixupHeadings()
+    val newContentLines = doc.rendered().lines().fixCodeIndent()
+
+    outDir.parentFile.mkdirs()
+    outDir.resolve("book.html").writeText(
+        newContentLines.joinToString("\n")
+    )
+    cssFile.copyTo(
+        outDir.resolve("theme/html/html.css").apply {
+            parentFile.mkdirs()
+        },
+        overwrite = true
+    )
+}
+
+private fun Document.fixupHeadings() = apply {
+    renameAllElements("h6", "h7")
+    renameAllElements("h5", "h6")
+    renameAllElements("h4", "h5")
+    renameAllElements("h3", "h4")
+    renameAllElements("h2", "h3")
+    renameNonChapterElements("h1", "h2")
+}
+
+private fun List<String>.fixBrokenTags(): List<String> = map { line ->
+    when {
+        line.trimStart().startsWith("<link ") -> line + "</link>"
+        line.trimStart().startsWith("<meta ") -> line + "</meta>"
+        line.contains("<img ") -> line.replace("<img (.*?)>".toRegex(), "<img $1 />")
+        else -> line
+    }
+}
+
+private fun String.toDocument(): Document =
+    DocumentBuilderFactory
+        .newInstance()
+        .newDocumentBuilder()
+        .parse(byteInputStream())
+
+private fun Document.renameNonChapterElements(from: String, to: String) {
+    getElementsByTagName(from).toList().forEach() {
+        val parentTypeAttribute = it.parentNode.attributes.getNamedItem("data-type")?.nodeValue
+        if (parentTypeAttribute != "chapter")
+            renameNode(it, null, to)
+    }
+}
+
+private fun Document.rendered(): String {
+    val result = StringWriter()
+    TransformerFactory.newInstance().newTransformer().transform(
+        DOMSource(this), StreamResult(result)
+    )
+    return result.toString()
+}
+
+private fun List<String>.fixCodeIndent(): List<String> = sequence {
+    val iterator = iterator()
+    while (iterator.hasNext()) {
+        val line = iterator.next()
+        when {
+            line.trimStart().startsWith("<pre data-code-language=") -> {
+                yield(line.trimStart())
+                yield(iterator.next().trimStart())
+            }
+            else -> yield(line)
+        }
+    }
+}.toList()
 
 private fun Document.renameAllElements(from: String, to: String) {
     getElementsByTagName(from).toList().forEach() {
         renameNode(it, null, to)
     }
 }
-
-private fun NamedNodeMap.toMap(): Map<String, Node> =
-    List<Node>(this.length) { index ->
-        this.item(index)
-    }.map { it.nodeName to it}.toMap()
-
-
 
 private fun NodeList.toList(): List<Node> = List<Node>(this.length) { index ->
     this.item(index)
