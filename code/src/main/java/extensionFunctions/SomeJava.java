d3847f7965233ab08cc169f02e955f7e1f2d5230
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-25 12:16:13 +0000

    More Functions to Extension Functions

--- code/src/main/java/extensionFunctions/SomeJava.java
+++ code/src/main/java/extensionFunctions/SomeJava.java
@@ -1,12 +1,40 @@
 package extensionFunctions;
 
-import static extensionFunctions.A.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import static extensionFunctions.Extension_functionsKt.nameForMarketing;
 
 public class SomeJava {
 
-    public static void code(Customer customer) {
+    public static void callingExt(Customer customer) {
         /// begin: callingExt
         var s = Extension_functionsKt.nameForMarketing(customer); // <1>
         /// end: callingExt
+    }
+
+    /// begin: toCustomer
+    static Customer toCustomer(JsonNode node) {
+        /// mute: toCustomer
+        return null;
+        /// resume: toCustomer
+    }
+    /// end: toCustomer
+
+    /// begin: customerFrom
+    static Customer customerFrom(JsonNode node) {
+        /// mute: customerFrom
+        return null;
+        /// resume: customerFrom
+    }
+    /// end: customerFrom
+
+    public static void callingUtil(JsonNode node) {
+        /// begin: calling
+        var customer = customerFrom(node);
+        var customer2 = toCustomer(node);
+        /// end: calling
+
+        /// begin: chaining
+        var marketingNameLength = nameForMarketing(customerFrom(node)).length();
+        /// end: chaining
     }
 }
--- code/src/main/java/extensionFunctions/extension-functions.kt
+++ code/src/main/java/extensionFunctions/extension-functions.kt
@@ -1,76 +1,106 @@
 package extensionFunctions
 
-import extensionFunctions.A.Customer
+import com.fasterxml.jackson.databind.JsonNode
 
-object A {
-    /// begin: customerFull
-    /// begin: customer
-    data class Customer(
-        val givenName: String,
-        val familyName: String
-    ) {
-        /// mute: customer
-        val fullName get() = "$givenName $familyName"
-        /// resume: customer
-    }
-    /// end: customer
-    /// end: customerFull
+/// begin: customerFull
+/// begin: customer
+data class Customer(
+    val id: String,
+    val givenName: String,
+    val familyName: String
+) {
+    /// mute: customer
+    val fullName get() = "$givenName $familyName"
+    /// resume: customer
+}
+/// end: customer
+/// end: customerFull
 
-    /// begin: fullName
-    fun fullName(customer: Customer) =
-        "$customer.givenName $customer.familyName"
-    /// end: fullName
+/// begin: fullName
+fun fullName(customer: Customer) =
+    "$customer.givenName $customer.familyName"
+/// end: fullName
 
+fun calling(customer: Customer) {
+    /// begin: callingExt
+    val s = customer.nameForMarketing()
+    /// end: callingExt
 
+    /*
+    /// begin: error
+    nameForMarketing(customer) // <1> doesn't compile
+    /// end: error
+     */
+    /// begin: references
+    val methodReference: (Customer.() -> String) =
+        Customer::fullName
+    val extensionFunctionReference: (Customer.() -> String) =
+        Customer::nameForMarketing
 
-    fun calling(customer: Customer) {
-        /// begin: callingExt
-        val s = customer.nameForMarketing()
-        /// end: callingExt
+    val methodAsFunctionReference: (Customer) -> String =
+        methodReference
+    val extensionAsFunctionReference: (Customer) -> String =
+        extensionFunctionReference
+    /// end: references
 
-        /*
-        /// begin: error
-        nameForMarketing(customer) // <1> doesn't compile
-        /// end: error
-         */
-        /// begin: references
-        val methodReference: (Customer.() -> String) = Customer::fullName
-        val extensionFunctionReference: (Customer.() -> String) = Customer::nameForMarketing
+    /// begin: invoke1
+    customer.methodReference()
+    customer.extensionFunctionReference()
 
-        val methodAsFunctionReference: (Customer) -> String = methodReference
-        val extensionAsFunctionReference: (Customer) -> String = extensionFunctionReference
-        /// end: references
+    methodAsFunctionReference(customer)
+    extensionAsFunctionReference(customer)
+    /// end: invoke1
 
-        /// begin: invoke1
-        customer.methodReference()
-        customer.extensionFunctionReference()
+    /// begin: invoke2
+    methodReference(customer)
+    extensionFunctionReference(customer)
+    /// end: invoke2
 
-        methodAsFunctionReference(customer)
-        extensionAsFunctionReference(customer)
-        /// end: invoke1
+    /*
+    /// begin: invoke3
+    customer.methodAsFunctionReference() // <1> doesn't compile
+    customer.extensionAsFunctionReference() // doesn't compile
+    /// end: invoke3
+     */
 
-        /// begin: invoke2
-        methodReference(customer)
-        extensionFunctionReference(customer)
-        /// end: invoke2
-
-        /*
-        /// begin: invoke3
-        customer.methodAsFunctionReference() // <1> doesn't compile
-        customer.extensionAsFunctionReference() // doesn't compile
-        /// end: invoke3
-         */
-    }
 }
 
 /// begin: nameForMarketingExt
 fun Customer.nameForMarketing() = "${familyName.toUpperCase()}, $givenName}"
 /// end: nameForMarketingExt
 
+object Chaining {
+    val jsonNode: JsonNode = SOME_CODE()
 
-object B {
+    /// begin: chaining
+    fun JsonNode.toCustomer(): Customer = SOME_CODE()
+
+    val nameForMarketingLength = jsonNode.toCustomer().nameForMarketing().length
+
+    /// end: chaining
+}
+class TitleProvider(val title: String) {
+    fun Customer.nameWithTitle() = "$title $fullName"
+}
+
+class Element {
+    fun attribute(s: String): String = SOME_CODE()
+}
+
+/// begin: xml
+fun Element?.toCustomer(): Customer? = this?.let { element ->
+    Customer(
+        element.attribute("id"),
+        element.attribute("first-name"),
+        element.attribute("last-name")
+    )
+}
+/// end: xml
+
+object CounterFactuals {
     /// begin: postalName
     data class Customer(
+        val id: String,
         val givenName: String,
         val familyName: String
     ) {
@@ -84,7 +114,6 @@ object B {
         "${customer.familyName.toUpperCase()}, $customer.givenName}"
     /// end: nameForMarketing
 
-    class TitleProvider(val title: String) {
-        fun Customer.nameWithTitle() = "$title $fullName"
-    }
 }
+
+fun <T> SOME_CODE(): T = TODO()
