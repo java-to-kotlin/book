228c54f5c472dde9954ab20c7bf9d10328758b8e
Duncan McGregor <duncan@oneeyedmen.com>
2021-05-21 23:42:23 +0100

    Smooth 13-encapsulated-collections-to-typealiases.ad

--- code/src/main/java/encapsulation/typealias.kt
+++ code/src/main/java/encapsulation/typealias.kt
@@ -5,7 +5,7 @@ import encapsulation.sendEmail
 import java.net.InetAddress
 
 /// begin: typealias
-typealias ISendEmail = (Email) -> Unit
+typealias EmailSenderFunction = (Email) -> Unit
 
 class EmailSender(
     /// mute: typealias
@@ -13,7 +13,7 @@ class EmailSender(
     private val username: String,
     private val password: String
     /// resume: typealias
-) : ISendEmail {
+) : EmailSenderFunction {
     override fun invoke(email: Email) {
         sendEmail(
             email,
